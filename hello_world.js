52be90b5fb2f23073623fd64ee35140ebd1cf45d
NatannnM <122119218+NatannnM@users.noreply.github.com>
2024-08-19 19:54:17 -0300

    Alterado mensagem de hello world para ser exibida em um paragrafo (#1)
    * Alterado mensagem de hello world para ser exibida em um paragrafo
    
    * limpar hello world quando não houver nome digitado

--- hello_world.js
+++ hello_world.js
@@ -1,4 +1,9 @@
 function helloworld(){
     const input = document.querySelector('#name');
-    alert("Bem vindo a programação móvel " + input.value);
+    const p = document.querySelector('#HelloWorldMessage');
+    if(input.value){
+        p.textContent = "Bem vindo a programação móvel " + input.value + "!";
+    }else{
+        p.textContent = '';
+    }    
 }
