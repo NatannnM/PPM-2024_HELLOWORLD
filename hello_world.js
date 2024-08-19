1b086217664899435d445263d970676cb96ea360
NatannnM <nata_martins@outlook.com>
2024-08-19 19:30:49 -0300

    Alterado mensagem de hello world para ser exibida em um paragrafo

--- hello_world.js
+++ hello_world.js
@@ -1,4 +1,7 @@
 function helloworld(){
     const input = document.querySelector('#name');
-    alert("Bem vindo a programação móvel " + input.value);
+    if(input.value){
+        const p = document.querySelector('#HelloWorldMessage');
+        p.textContent = "Bem vindo a programação móvel " + input.value + "!";
+    }    
 }
